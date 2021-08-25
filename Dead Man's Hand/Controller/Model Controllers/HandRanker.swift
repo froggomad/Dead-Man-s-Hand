b012af5ea70f9eec7eedf23108be9632dff4c1be
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-25 12:01:32 -0700

    moved logic from `Hand` to `HandRanker`

--- Dead Man's Hand/Controller/Model Controllers/HandRanker.swift
+++ Dead Man's Hand/Controller/Model Controllers/HandRanker.swift
@@ -21,18 +21,18 @@ struct HandRanker {
               let hand2 = player2.hand
         else { return .tie }
         
-        let highHand1 = hand1.rankHand()
-        let highHand2 = hand2.rankHand()
+        let highHand1 = rankHand(hand1)
+        let highHand2 = rankHand(hand2)
         
         if highHand1.rawValue > highHand2.rawValue {
             return .win(rank: highHand1, player: player1)
             // highHand tied
         } else if highHand1.rawValue == highHand2.rawValue {
             // check for next highest card that doesn't tie
-            if hand1.highCard() > hand2.highCard() {
+            if highestCard(hand1) > highestCard(hand2) {
                 return .win(rank: highHand1, player: player1)
-            } else if player1.hand!.highCard() == player2.hand!.highCard() {
-                var highCard = hand1.highCard()
+            } else if highestCard(hand1) == highestCard(hand2) {
+                var highCard = highestCard(hand1)
                 
                 for i in (0...3).reversed() {
                     if hand1.cards[i] > hand2.cards[i] {
@@ -42,10 +42,10 @@ struct HandRanker {
                     }
                 }
                 
-                if highCard == hand2.highCard() {
+                if highCard == highestCard(hand2) {
                     return .tie
                 } else {
-                    return highCard > hand1.highCard() ? .win(rank: highHand2, player: player2) : .win(rank: highHand1, player: player1)
+                    return highCard > highestCard(hand1) ? .win(rank: highHand2, player: player2) : .win(rank: highHand1, player: player1)
                 }
             } else {
                 return .win(rank: highHand2, player: player2)
@@ -53,6 +53,94 @@ struct HandRanker {
         }
         return .tie // edge case
     }
+    
+    /// Rank a player's Hand
+    func rankHand(_ hand: Hand) -> WinningHandType {
+        if isFlush(hand) {
+            if isStraight(hand) {
+                return .straightFlush
+            }
+            return .flush
+        } else if isStraight(hand) {
+            return .straight
+        } else {
+            // this is performed last even though some hands rank
+            // higher than straight and flush. Due to a straight or
+            // flush ruling out any possibilities this method produces
+            return findRankMatches(hand)
+        }
+        
+    }
+    /// Returns the highest ranking winning hand out of the player's hand
+    ///
+    /// Possibilities:
+    /// - high card
+    /// - pair
+    /// - two pair
+    /// - 3 of a kind
+    /// - full house
+    /// - four of a kind
+    private func findRankMatches(_ hand: Hand) -> WinningHandType {
+        let tupleArray = hand.cards.map { ($0.rank, 1) }
+        
+        let rankFrequencyMap = Dictionary(tupleArray, uniquingKeysWith: +)
+        let max = rankFrequencyMap.max(by: { $0.value < $1.value })?.value
+        
+        guard let max = max else { return .highCard }
+        
+        switch max {
+        case 4:
+            return .fourOfAKind
+        case 3:
+            if rankFrequencyMap.contains(where: {$0.value == 2}) {
+                return .fullHouse
+            }
+            return .threeOfAKind
+        case 2:
+            for (key, value) in rankFrequencyMap {
+                if value == 2 {
+                    for (secondKey, value) in rankFrequencyMap {
+                        if value == 2 && key != secondKey {
+                            return .twoPair
+                        }
+                    }
+                }
+            }
+            return .pair
+        default:
+            return .highCard
+        }
+    }
+    
+    private func isFlush(_ hand: Hand) -> Bool {
+        let suits = hand.cards.map({$0.suit})
+        return suits.allSatisfy({ $0 == suits.first })
+    }
+    
+    private func isStraight(_ hand: Hand) -> Bool {
+        let ranks = hand.cards.map { $0.rank }
+        var lastRank = ranks[0].rawValue
+        var consecutiveCards = 1
+        
+        for (i, rank) in ranks.enumerated() {
+            if i != 0 { // lastRank contains first element
+                if rank.rawValue == lastRank + 1 {
+                    consecutiveCards += 1
+                } else {
+                    break // exit early since this isn't a straight
+                }
+            }
+            lastRank = rank.rawValue
+        }
+        
+        return consecutiveCards == 5
+    }
+    
+    func highestCard(_ hand: Hand) -> Card {
+        let max = hand.cards.max(by: { $0.rank.rawValue < $1.rank.rawValue})!
+        return max
+    }
+    
 }
 
 enum WinningHand {
