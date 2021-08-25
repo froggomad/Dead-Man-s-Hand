e33f397dc096bf72aa3b831b4ae091efe8a2e433
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-25 00:22:42 -0700

    Initial project organization

---
+++ Dead Man's Hand/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dead Man's Hand
+//
+//  Created by Kenneth Dubroff on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
