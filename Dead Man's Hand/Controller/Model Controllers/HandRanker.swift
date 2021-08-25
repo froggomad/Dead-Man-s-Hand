fe708dc5559840b5a747e6fefee887c2f333cfb2
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-25 10:41:13 -0700

    Changed ranker logic to return either a win or tie state - began implementing tie

--- Dead Man's Hand/Controller/Model Controllers/HandRanker.swift
+++ Dead Man's Hand/Controller/Model Controllers/HandRanker.swift
@@ -16,21 +16,31 @@ struct HandRanker {
         self.player2 = player2
     }
     
-    func highHand() -> (rank: WinningHandType, player: Player) {
+    func highHand() -> WinningHand {
         // TODO: force-unwrapped
         let highHand1 = player1.hand!.rankHand()
         let highHand2 = player2.hand!.rankHand()
+        
         if highHand1.rawValue > highHand2.rawValue {
-            return (rank: highHand1, player: player1)
+            return .win(rank: highHand1, player: player1)
+            // highHand tied, check for next highest card that doesn't tie
         } else if highHand1.rawValue == highHand2.rawValue {
-            if player1.hand!.highCard().rank > player2.hand!.highCard().rank {
-                return (rank: highHand1, player: player1)
-                // TODO: handle ties
+            
+            if player1.hand!.highCard() > player2.hand!.highCard() {
+                return .win(rank: highHand1, player: player1)
+            } else if player1.hand!.highCard() == player2.hand!.highCard() {
+                return .tie
             } else {
-                return (rank: highHand2, player: player2)
+                return .win(rank: highHand2, player: player2)
             }
+            
         } else {
-            return (rank: highHand2, player: player2)
+            return .win(rank: highHand2, player: player2)
         }
     }
+}
+
+enum WinningHand {
+    case win(rank: WinningHandType, player: Player)
+    case tie
 }
