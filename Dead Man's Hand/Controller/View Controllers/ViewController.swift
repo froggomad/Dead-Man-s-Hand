cc771edf400921b9be507ca962774638192d5ebf
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-26 11:07:05 -0700

    added live test for dealing a hand and displaying the player's cards

--- Dead Man's Hand/Controller/View Controllers/ViewController.swift
+++ Dead Man's Hand/Controller/View Controllers/ViewController.swift
@@ -16,13 +16,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let cardView = CardView(card: Card(suit: .clubs, rank: .ace))
-        cardView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(cardView)
-        NSLayoutConstraint.activate([
-            cardView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            cardView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
-        ])
+        testDealHand()
+    }
+    
+    private func testDealHand() {
+        let game = PokerGame()
+        game.drawHands()
+        
+        game.player1.hand!.cards.enumerated().forEach { index, card in
+            let cardView = CardView(card: card)
+            cardView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(cardView)
+            
+            let spacing = CGFloat(4*index)
+            NSLayoutConstraint.activate([
+                cardView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+                cardView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: CardView.cardWidth * CGFloat(index) + spacing)
+            ])
+        }
+        
+        let result = game.playHands()
+        switch result {
+        case let .win(rank, player):
+            print("Player: \(player.name) wins with a \(rank.description)")
+        case .tie:
+            print("tied!")
+        }
+        print("Opponent's hand: \(game.ranker.rankHand(game.player2.hand!))", "with high card: \(game.ranker.highestCard(game.player2.hand!).rank)")
     }
 
 }
